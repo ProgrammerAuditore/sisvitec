d8880dd2e31bdcd3b10dfdb53443b9140fd59a13
ProgrammerAuditore <victorvj098@gmail.com>
2022-11-07 18:17:29 -0600

    *** maximo : Se agrego mayusculas en la codificacion y decodificacion de palabras

--- conexion.php
+++ conexion.php
@@ -1,33 +1,47 @@
 <?php
-    class Conexion
+class Conexion
+{
+    private $conn;
+    function __construct()
     {
-        private $conn;
-        function __construct()
-        {
-            $this->conn=new mysqli("localhost","root","","ceuarkos_sisvintec", 3306);
-            // $this->conn=new mysqli("v196512","ceuarkos_SisVinTec","#~AI,w6oONSd","ceuarkos_SisvinTec");
-        }
-        function _ObtenerConexion()
-        {
-            return  $this->conn;
-        }
+        $this->conn = new mysqli("localhost", "root", "", "ceuarkos_sisvintec", 3306);
+        // $this->conn=new mysqli("v196512","ceuarkos_SisVinTec","#~AI,w6oONSd","ceuarkos_SisvinTec");
     }
-    $words_mex_encode = array(
-        "ñ" => "%n%",
-        "ü" => "%uu%", 
-        "á" => "%a%", 
-        "é" => "%e%", 
-        "í" => "%i%", 
-        "ó" => "%o%", 
-        "ú" => "%u%"
-    );
-    
-    $words_mex_decode = array(
-        "%n%" => "ñ",
-        "%uu%" => "ü", 
-        "%a%" => "á", 
-        "%e%" => "é", 
-        "%i%" => "í", 
-        "%o%" => "ó", 
-        "%u%" => "ú"
-    );
+    function _ObtenerConexion()
+    {
+        return  $this->conn;
+    }
+}
+$words_mex_encode = array(
+    "ñ" => "%n%",
+    "ü" => "%-u%",
+    "á" => "%a%",
+    "é" => "%e%",
+    "í" => "%i%",
+    "ó" => "%o%",
+    "ú" => "%u%",
+    "Ñ" => "%N%",
+    "Ü" => "%-U%",
+    "Á" => "%A%",
+    "É" => "%E%",
+    "Í" => "%I%",
+    "Ó" => "%O%",
+    "Ú" => "%U%"
+);
+
+$words_mex_decode = array(
+    "%n%" => "ñ",
+    "%-u%" => "ü",
+    "%a%" => "á",
+    "%e%" => "é",
+    "%i%" => "í",
+    "%o%" => "ó",
+    "%u%" => "ú",
+    "%N%" => "Ñ",
+    "%-U%" => "Ü",
+    "%A%" => "Á",
+    "%E%" => "É",
+    "%I%" => "Í",
+    "%O%" => "Ó",
+    "%U%" => "Ú"
+);
